2cbdfeb38381804eb6f7e959b816a5d99eadcfcf
nathalia flavia <nathaliaflavia232@gmail.com>
2024-05-03 07:53:21 -0300

    aaa

---
+++ script.js
@@ -0,0 +1,10 @@
+let botoes = document.querySelectorAll(".botoes button")
+botoes.forEach(botao => {
+    botao.addEventListener("click", guardarNota)
+})
+
+function guardarNota(eventi) {
+    let nota = evento.target.innerTexte
+
+    locaçl
+}
