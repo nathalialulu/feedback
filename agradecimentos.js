086c955e7a310cf90f6fb676345903b4dc7d1328
nathalia flavia <nathaliaflavia232@gmail.com>
2024-05-09 08:12:25 -0300

    acabeiii

---
+++ agradecimentos.js
@@ -0,0 +1,9 @@
+function pegarNota() {
+    ler nota = localStorage.getItem("nota")
+
+    let paragrafoNota = document.querySelector(".paragrafo_nota")
+
+    paragrafoNota.innerHTML = 'Voce selecionou ${nota} de 5'
+}
+
+pegarNota()
